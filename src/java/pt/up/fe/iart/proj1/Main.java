61d13df52d1fb09f472d63c3a829e371580c66fd
migulorama <migulorama@gmail.com>
2014-04-09 11:11:11 +0100

    Add scala module to project.

--- src/java/pt/up/fe/iart/proj1/Main.java
+++ src/java/pt/up/fe/iart/proj1/Main.java
@@ -3,7 +3,7 @@ package pt.up.fe.iart.proj1;
 public class Main {
 
     public static void main(String[] args) {
-        
+        System.out.println("Hello World!");
     }
 
 }
---
+++ src/scala/pt/up/fe/iart/proj1/collections/Graph.scala
@@ -0,0 +1,8 @@
+package pt.up.fe.iart.proj1.collections
+
+/**
+ * Created by miguel on 09/04/2014.
+ */
+class Graph {
+
+}
