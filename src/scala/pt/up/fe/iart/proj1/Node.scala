57025ecb03ad4a62097b55b99a1eafc52094df08
migulorama <migulorama@gmail.com>
2014-04-16 19:08:13 +0100

    Implement Search Algorithms (AStar, BestFirst, BranchAndBound, BreadthFirst, DepthFirst, DepthLimited and IterativeDeepening)

---
+++ src/scala/pt/up/fe/iart/proj1/Node.scala
@@ -0,0 +1,33 @@
+package pt.up.fe.iart.proj1
+
+import scala.annotation.tailrec
+
+class Node[S, A](val state: S, val parent: Option[Node[S, A]], val action: Option[A], val depth: Int, val pathCost: Double) {
+
+    def solution: List[A] = {
+        @tailrec
+        def loop(node: Node[S, A], a: List[A]): List[A] =
+            node.parent match {
+                case None => a
+                case Some(x) => loop(x, node.action.get :: a)
+            }
+        loop(this, List.empty)
+    }
+}
+
+/* Factory for easy Node creation */
+object Node {
+    def apply[S,A](state:S) = new Node[S,A](state,None,None,0,0.0)
+
+    def apply[S,A](state: S, parent: Option[Node[S,A]], action: Option[A], depth: Int) =
+        new Node[S,A](state,parent,action,depth,0.0)
+
+    def apply[S,A](state: S, parent: Option[Node[S,A]], action: Option[A], depth: Int, pathCost: Double) =
+        new Node[S,A](state,parent,action,depth,pathCost)
+
+    def childNode[S,A](problem: Problem[S,A], parent: Node[S,A], action: A) = {
+        val state = problem.result(parent.state,action)
+        val pathCost = parent.pathCost + problem.stepCost(parent.state,action,state)
+        new Node[S,A](state,Some(parent),Some(action),parent.depth+1,pathCost)
+    }
+}
---
+++ src/scala/pt/up/fe/iart/proj1/Search.scala
@@ -0,0 +1,107 @@
+package pt.up.fe.iart.proj1
+
+import pt.up.fe.iart.proj1.collections.QueueLike
+import scala.annotation.tailrec
+import scala.collection.mutable
+
+/* Possible search results that various search algorithms can return */
+sealed abstract class SearchResult[A]
+
+case class Success[A](result: A) extends SearchResult[A]
+
+case class Failure[A]() extends SearchResult[A]
+
+case class CutOff[A]() extends SearchResult[A]
+
+object GraphSearch {
+    def apply[S, A](problem: Problem[S, A], frontier: QueueLike[Node[S, A]]) = {
+        @tailrec
+        def loop(frontier: QueueLike[Node[S, A]], explored: scala.collection.immutable.HashSet[S]): SearchResult[List[A]] =
+            frontier.removeFirst() match {
+                case None => Failure()
+                case Some(node) if problem.goalTest(node.state) =>
+                    Success(node.solution)
+                case Some(node) =>
+                    if (explored.exists(_ == node.state))
+                        loop(frontier, explored)
+                    else {
+                        val acts = problem.actions(node.state)
+                        acts.foreach((a: A) => frontier.insert(Node.childNode(problem, node, a)))
+                        loop(frontier, explored + node.state)
+                    }
+            }
+
+        loop(frontier.insert(Node(problem.initialState)), scala.collection.immutable.HashSet.empty)
+    }
+}
+
+object AStarSearch {
+    def apply[S, A](problem: Problem[S, A]): SearchResult[List[A]] =
+        GraphSearch(problem, new scala.collection.mutable.PriorityQueue[Node[S, A]]()(Ordering.by(AStarHeuristic(_, problem))))
+
+    private def AStarHeuristic[S, A](node: Node[S, A], problem: Problem[S, A]) =
+        node.pathCost + problem.estimatedCostToGoal(node.state) //f(n) = g(n) + h(n)
+}
+
+object BestFirstSearch {
+    def apply[S, A](problem: Problem[S, A]): SearchResult[List[A]] =
+        GraphSearch(problem, new scala.collection.mutable.PriorityQueue[Node[S, A]]()(Ordering.by(BestFirstHeuristic(_, problem))))
+
+    private def BestFirstHeuristic[S, A](node: Node[S, A], problem: Problem[S, A]) =
+        problem.estimatedCostToGoal(node.state) //f(n) = h(n)
+}
+
+object BranchAndBoundSearch {
+    def apply[S, A](problem: Problem[S, A]): SearchResult[List[A]] =
+        GraphSearch(problem, new scala.collection.mutable.PriorityQueue[Node[S, A]]()(Ordering.by(BranchAndBoundHeuristic(_, problem))))
+
+    private def BranchAndBoundHeuristic[S, A](node: Node[S, A], problem: Problem[S, A]) =
+        node.pathCost //f(n) = g(n)
+}
+
+object BreadthFirstSearch {
+    def apply[S, A](problem: Problem[S, A]): SearchResult[List[A]] =
+        GraphSearch(problem, new mutable.Queue[Node[S, A]]())
+}
+
+object DepthFirstSearch {
+    def apply[S, A](problem: Problem[S, A]): SearchResult[List[A]] =
+        GraphSearch(problem, new mutable.Stack[Node[S, A]]())
+}
+
+object DepthLimitedSearch {
+    def apply[S, A](problem: Problem[S, A], limit: Int) =
+        recursiveDLS(Node[S, A](problem.initialState), problem, limit)
+
+    private def recursiveDLS[S, A](node: Node[S, A], problem: Problem[S, A], limit: Int): SearchResult[List[A]] = {
+        if (problem.goalTest(node.state))
+            Success(node.solution)
+        else if (node.depth == limit)
+            CutOff()
+        else {
+            def loop(nodes: List[Node[S, A]], cutoffOccured: Boolean): SearchResult[List[A]] =
+                nodes match {
+                    case Nil if cutoffOccured => CutOff()
+                    case Nil => Failure()
+                    case n :: rest =>
+                        recursiveDLS(n, problem, limit) match {
+                            case Failure() => loop(rest, cutoffOccured)
+                            case CutOff() => loop(rest, true)
+                            case ret => ret
+                        }
+                }
+            loop(problem.actions(node.state).map(Node.childNode(problem, node, _)), false)
+        }
+    }
+}
+
+object IterativeDeepeningSearch {
+    def apply[S, A](problem: Problem[S, A]) = {
+        def loop(depth: Int): SearchResult[List[A]] =
+            DepthLimitedSearch(problem, depth) match {
+                case CutOff() => loop(depth + 1)
+                case res => res
+            }
+        loop(0)
+    }
+}
